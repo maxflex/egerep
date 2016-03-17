d25337b90673fad553daca73974169244c1f7d6a
Max Kolyadin <makcyxa-k@yandex.ru>
2016-03-17 15:11:28 +0300

    Word boundary added

--- public/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $string = "1234567-7654321, sdfdsf";
-
-    preg_match_all("/(\b[\d]{7}\b)/", $string, $output_array);
-
-    echo '<pre>';
-    print_r($output_array);
-    echo '</pre>';
