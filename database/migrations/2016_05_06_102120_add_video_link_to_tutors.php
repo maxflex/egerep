2e63abc4ce40a2cfab5f728185b2beb674bc639f
shamshod <shamik1551@mail.ru>
2016-05-06 10:46:37 +0300

    - tutor video link

---
+++ database/migrations/2016_05_06_102120_add_video_link_to_tutors.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVideoLinkToTutors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tutors', function (Blueprint $table) {
+            $table->string('video_link',20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tutors', function (Blueprint $table) {
+            //
+        });
+    }
+}
