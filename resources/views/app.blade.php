73196fea7db083e0023908ce3670a863905a5a88
Max Kolyadin <makcyxa-k@yandex.ru>
2016-06-01 16:34:27 +0300

    Debt & accounts privacy settings

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,8 +39,13 @@
               <a href="requests" class="list-group-item">Заявки</a>
               <a href="attachments" class="list-group-item">Стыковки</a>
               <a href="tutors" class="list-group-item">Преподаватели</a>
-              <a href="periods" class="list-group-item">Расчеты</a>
-              <a href="debt/map" class="list-group-item">Дебет</a>
+              @if (\App\Models\User::fromSession()->show_accounts)
+                  <a href="periods" class="list-group-item">Расчеты</a>
+              @endif
+
+              @if (\App\Models\User::fromSession()->show_debt)
+                  <a href="debt/map" class="list-group-item">Дебет</a>
+              @endif
               <a href="summary" class="list-group-item">Итоги</a>
               <a href="logout" class="list-group-item">Выход</a></div>
         </div>
