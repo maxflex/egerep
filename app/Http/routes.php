2d77ba0b7114d0ff7187a92e7f17b4d44dd15134
Max Kolyadin <makcyxa-k@yandex.ru>
2016-06-02 13:12:00 +0300

    Clients search

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,8 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('client/{id}', function($id) {
         $request_id = \App\Models\Request::where('client_id', $id)->value('id');
         if ($request_id) {
-            return redirect()->route('requests.edit', $request_id);
+            return redirect()->to("https://lk.a-perspektiva.ru/requests/{$request_id}/edit");
+            // return redirect()->route('requests.edit', $request_id);
         } else {
             return redirect()->to('/');
         }
