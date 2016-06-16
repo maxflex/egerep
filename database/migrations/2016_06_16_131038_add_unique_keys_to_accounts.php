46b0e5c9932ecc567fe09b6866c07d2681fe6b00
shamshod <shamik1551@mail.ru>
2016-06-16 17:02:11 +0300

    - renamed

--- database/migrations/2016_06_16_131038_add_unique_keys_to_accounts.php
+++ database/migrations/2016_06_16_131038_add_unique_keys_to_accounts.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUniqueKeys extends Migration
+class AddUniqueKeysToAccounts extends Migration
 {
     /**
      * Run the migrations.
