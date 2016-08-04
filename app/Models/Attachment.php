0d93af8c172091c95a1ef2c54c71e0358489bbbd
Max Kolyadin <makcyxa-k@yandex.ru>
2016-08-04 11:51:28 +0300

    Omtimize errors script 5

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -423,11 +423,11 @@ class Attachment extends Model
 
             if (! $archive->total_lessons_missing) {
                 if ($this->hide) {
-                    if (($this->account_data_count && $archive->getClean('date') == $last_lesson_date) || (! $this->account_data_count && $archive_date->diff($attachment_date)->format("%a") == 7)) {
+                    if (($this->account_data_count && $archive->getClean('date') != $last_lesson_date) || (! $this->account_data_count && $archive_date->diff($attachment_date)->format("%a") != 7)) {
                         $errors[] = 13;
                     }
                 } else {
-                    if (($this->account_data_count && $archive->getClean('date') != $last_lesson_date) || (! $this->account_data_count && $archive_date->diff($attachment_date)->format("%a") != 7)) {
+                    if (($this->account_data_count && $archive->getClean('date') == $last_lesson_date) || (! $this->account_data_count && $archive_date->diff($attachment_date)->format("%a") == 7)) {
                         $errors[] = 14;
                     }
                 }
