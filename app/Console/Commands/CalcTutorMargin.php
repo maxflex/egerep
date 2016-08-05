6b52e2e8952a4e271876692fd3c78114273adad0
Max Kolyadin <makcyxa-k@yandex.ru>
2016-08-05 16:38:07 +0300

    Tutor margin

---
+++ app/Console/Commands/CalcTutorMargin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Tutor;
+use DB;
+
+class CalcTutorMargin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'calc:tutor_margin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Recalculate tutor margin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // выбираем только тех репетиторов, у которых есть хотя бы минимум 4 скрытых клиента
+        $tutor_ids = DB::table('tutors')->whereRaw("(SELECT COUNT(*) FROM attachments WHERE tutor_id = tutors.id AND hide=1) >= 4")->pluck('id');
+
+        $bar = $this->output->createProgressBar(count($tutor_ids));
+
+        foreach($tutor_ids as $tutor_id) {
+            $hidden_client_ids = DB::table('attachments')->where('hide', 1)->where('tutor_id', $tutor_id)->pluck('client_id');
+            $total_commission = DB::table('account_datas')->whereIn('client_id', $hidden_client_ids)->sum('commission');
+            DB::table('tutors')->where('id', $tutor_id)->update(['margin' => round($total_commission / count($hidden_client_ids))]);
+            $bar->advance();
+        }
+
+        $bar->finish();
+    }
+}
---
+++ database/migrations/2016_08_05_155612_add_margin_to_tutors.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMarginToTutors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tutors', function (Blueprint $table) {
+            $table->integer('margin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tutors', function (Blueprint $table) {
+            //
+        });
+    }
+}
