b166d76625202c6d429722c6b8dac4b9a54e0663
Max Kolyadin <makcyxa-k@yandex.ru>
2016-08-05 18:06:10 +0300

    Merge margin 1

--- app/Models/Tutor.php
+++ app/Models/Tutor.php
@@ -164,7 +164,11 @@ class Tutor extends Model
     public function getStatisticsAttribute()
     {
         $stats = Api\Api::exec('teacherStatistics', ['tutor_id' => $this->id]);
-        
+
+        if (! $stats) {
+            $stats = new stdClass();
+        }
+
         $stats->er_review_count = Attachment::where('tutor_id', $this->id)->has('review')->count();
         $review_score_sum = DB::table('reviews')->join('attachments', 'attachments.id', '=', 'attachment_id')->where('tutor_id', $this->id)->select('reviews.score')->sum('reviews.score');
         $stats->er_review_avg = (4*($this->lk + $this->tb + ($this->js < 8 ?$this->js : 8 - $this->js % 8)) + $review_score_sum)/(4 + $stats->er_review_count);
