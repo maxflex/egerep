b0e43f023b90fa21c2946f7ade7accc55c8c8e54
Max Kolyadin <makcyxa-k@yandex.ru>
2016-08-05 18:49:40 +0300

    Merge margin 7

--- resources/views/tutors/_form.blade.php
+++ resources/views/tutors/_form.blade.php
@@ -308,10 +308,10 @@
             <h4>СТАТИСТИКА</h4>
             <div>Количество клиентов: @{{ tutor.attachments_count }}</div>
             <div ng-show="tutor.margin">Группа маржинальности: М@{{ tutor.margin }}</div>
-            <div>Количество оценок и средняя оценка: @{{ tutor.statistics.er_review_count }} | @{{ tutor.statistics.er_review_avg | number : 1 }}</div>
+            <div>Cредняя оценка: @{{ tutor.statistics.ec_review_avg | number : 1 }} (на основе <plural count='tutor.statistics.ec_review_count' type='mark'></plural>)</div>
             <div ng-show="tutor.in_egecentr">Количество занятий, проведенных в ЕГЭ-Центре: @{{ tutor.statistics.ec_lesson_count }}</div>
             <div ng-show="tutor.in_egecentr">Cредняя оценка: @{{ tutor.statistics.ec_review_avg | number : 1 }} (на основе <plural count='tutor.statistics.ec_review_count' type='mark'></plural>)</div>
-        </div> 
+        </div>
         <div ng-if="tutor.id">
             <h4>КОММЕНТАРИИ</h4>
             <comments entity-type='tutor' entity-id='{{ isset($id) ? $id : 0 }}' user='{{ $user }}'></comments>
