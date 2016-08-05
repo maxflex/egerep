e0699ec7c01a737f9c79e0314a32a3da6459bfc0
Max Kolyadin <makcyxa-k@yandex.ru>
2016-08-05 13:17:58 +0300

    Review errors Job

--- app/Jobs/RecalcReviewErrors.php
+++ app/Jobs/RecalcReviewErrors.php
@@ -29,6 +29,6 @@ class RecalcReviewErrors extends Job implements ShouldQueue
      */
     public function handle()
     {
-        \Artisan::call('calc:attachment_errors --reviews');
+        \Artisan::call('calc:model_errors --reviews');
     }
 }
