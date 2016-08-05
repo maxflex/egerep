f8c4eb402ab63d9dcfb024dec1d612fdbfe3f237
Max Kolyadin <makcyxa-k@yandex.ru>
2016-08-05 19:03:08 +0300

    Merge margin 10

--- resources/views/tutors/_form.blade.php
+++ resources/views/tutors/_form.blade.php
@@ -308,9 +308,17 @@
             <h4>СТАТИСТИКА</h4>
             <div>Количество клиентов: @{{ tutor.attachments_count }}</div>
             <div ng-show="tutor.margin">Группа маржинальности: М@{{ tutor.margin }}</div>
-            <div>Cредняя оценка: @{{ tutor.statistics.er_review_avg | number : 1 }} (на основе <plural count='tutor.statistics.er_review_count' type='mark'></plural>)</div>
+            <div>Cредняя оценка: @{{ tutor.statistics.er_review_avg | number : 1 }} (на основе @{{ tutor.statistics.er_review_count }} <ng-pluralize count='tutor.statistics.er_review_count' when="{
+                'one': 'оценки',
+                'few': 'оценок',
+                'many': 'оценок'
+            }"></ng-pluralize>)</div>
             <div ng-show="tutor.in_egecentr">Количество занятий, проведенных в ЕГЭ-Центре: @{{ tutor.statistics.ec_lesson_count }}</div>
-            <div ng-show="tutor.in_egecentr">Cредняя оценка: @{{ tutor.statistics.ec_review_avg | number : 1 }} (на основе <plural count='tutor.statistics.ec_review_count' type='mark'></plural>)</div>
+            <div ng-show="tutor.in_egecentr">Cредняя оценка в ЕГЭ-Центре: @{{ tutor.statistics.ec_review_avg | number : 1 }} (на основе @{{ tutor.statistics.ec_review_count }} <ng-pluralize count='tutor.statistics.ec_review_count' when="{
+                'one': 'оценки',
+                'few': 'оценок',
+                'many': 'оценок'
+            }"></ng-pluralize>)</div>
         </div>
         <div ng-if="tutor.id">
             <h4>КОММЕНТАРИИ</h4>
