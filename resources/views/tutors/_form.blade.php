1a4037800825a77841fce159a5057c97d5b7a66f
Max Kolyadin <makcyxa-k@yandex.ru>
2016-08-09 17:20:26 +0300

    JS fix

--- resources/views/tutors/_form.blade.php
+++ resources/views/tutors/_form.blade.php
@@ -313,14 +313,14 @@
                 'few': 'оценок',
                 'many': 'оценок'
             }"></ng-pluralize> от учеников и нашей оценки)</div>
-            <div ng-show="tutor.in_egecentr">Количество занятий, проведенных в ЕГЭ-Центре: @{{ tutor.statistics.ec_lesson_count }}</div>
-            <div ng-show="tutor.in_egecentr">Cредняя оценка в ЕГЭ-Центре: @{{ tutor.statistics.ec_review_avg | number : 1 }} (на основе @{{ tutor.statistics.ec_review_count }} <ng-pluralize count='tutor.statistics.ec_review_count' when="{
+            <div ng-show="tutor.statistics.ec_lesson_count">Количество занятий, проведенных в ЕГЭ-Центре: @{{ tutor.statistics.ec_lesson_count }}</div>
+            <div ng-show="tutor.statistics.ec_review_count">Cредняя оценка в ЕГЭ-Центре: @{{ tutor.statistics.ec_review_avg | number : 1 }} (на основе @{{ tutor.statistics.ec_review_count }} <ng-pluralize count='tutor.statistics.ec_review_count' when="{
                 'one': 'оценки',
                 'few': 'оценок',
                 'many': 'оценок'
             }"></ng-pluralize> от учеников и нашей оценки)</div>
         </div>
-        <div ng-if="tutor.id"> 
+        <div ng-if="tutor.id">
             <h4>КОММЕНТАРИИ</h4>
             <comments entity-type='tutor' entity-id='{{ isset($id) ? $id : 0 }}' user='{{ $user }}'></comments>
         </div>
