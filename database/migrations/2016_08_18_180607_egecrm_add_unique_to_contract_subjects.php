20100e896df7b0ce9d5f27609fd030968a93cc9f
shamshod <shamik1551@mail.ru>
2016-08-18 18:11:06 +0300

    #1205 - egecrm migration: add unique to contract_subjects table

---
+++ database/migrations/2016_08_18_180607_egecrm_add_unique_to_contract_subjects.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EgecrmAddUniqueToContractSubjects extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('egecrm')->table('contract_subjects', function (Blueprint $table) {
+            $table->unique(['id_contract', 'id_subject']);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
