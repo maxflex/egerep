c4c93af22953d084bddd0e61ae5adf3b689e84c2
Max Kolyadin <makcyxa-k@yandex.ru>
2016-08-23 15:55:10 +0300

    Rename column

--- resources/views/_menu.blade.php
+++ resources/views/_menu.blade.php
@@ -16,8 +16,8 @@
 @endif
 <a class="list-group-item active">Административное</a>
 <a href="summary" class="list-group-item">Итоги</a>
-@if (\App\Models\User::fromSession()->show_attachments)
-    <a href="attachments/stats" class="list-group-item">Стыковки</a>
+@if (\App\Models\User::fromSession()->show_stats)
+    <a href="attachments/stats" class="list-group-item">Статистика</a>
 @endif
 <a href="logs" class="list-group-item">Логи</a>
 <a href="logout" class="list-group-item">Выход</a>
