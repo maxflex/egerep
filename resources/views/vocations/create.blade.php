9824fb1b199307e809753c667504908fd39f1475
Max Kolyadin <makcyxa-k@yandex.ru>
2016-09-02 15:07:13 +0300

    Days count in vocations

--- resources/views/vocations/create.blade.php
+++ resources/views/vocations/create.blade.php
@@ -6,7 +6,12 @@
     @include('vocations._form')
     <div class="row" style="margin-top: 15px">
         <div class="col-sm-12 center">
-            <button class="btn btn-primary" ng-click='create()' ng-disabled='saving'>создать заявку на отпуск</button>
+            <button class="btn btn-primary" ng-click='create()' ng-disabled='saving || !vocation.data.length'>
+                создать заявку
+            </button>
+            <div class='text-gray small' style='margin-top: 6px; height: 8px'>
+                <plural ng-show='vocation.data.length' count='vocation.data.length' type='day'></plural>
+            </div>
         </div>
     </div>
 @stop
