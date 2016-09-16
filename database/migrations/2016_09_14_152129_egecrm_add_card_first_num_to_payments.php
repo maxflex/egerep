cae4936d1255f30ade662d1f2ef59f533b4bca47
shamshod <shamik1551@mail.ru>
2016-09-16 10:54:33 +0300

    - egecrm migration payments table card_first_number column added

---
+++ database/migrations/2016_09_14_152129_egecrm_add_card_first_num_to_payments.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EgecrmAddCardFirstNumToPayments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('egecrm')->table('payments', function (Blueprint $table) {
+            $table->enum('card_first_number', [4, 5, 6])->default(4);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
