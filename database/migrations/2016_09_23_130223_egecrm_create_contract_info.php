89d365fafcfa815bc689260d097b223632d41e42
Max Kolyadin <makcyxa-k@yandex.ru>
2016-09-23 17:12:18 +0300

    EGECRM contract_info migration

--- database/migrations/2016_09_23_130223_egecrm_create_contract_info.php
+++ database/migrations/2016_09_23_130223_egecrm_create_contract_info.php
@@ -12,11 +12,26 @@ class EgecrmCreateContractInfo extends Migration
      */
     public function up()
     {
-        Schema::create('contract_info', function (Blueprint $table) {
+        Schema::connection('egecrm')->create('contract_info', function (Blueprint $table) {
             $table->integer('id_contract')->unsigned()->primary();
             $table->integer('grade')->unsigned()->index();
             $table->integer('id_student')->unsigned()->index();
             $table->integer('year')->unsigned()->index();
+        });
+        $data = \DB::connection('egecrm')->table('contracts')->whereRaw('id=id_contract')->get();
+        foreach($data as $d) {
+            \DB::connection('egecrm')->table('contract_info')->insert([
+                'id_contract' => $d->id,
+                'grade'       => $d->grade,
+                'id_student'  => $d->id_student,
+                'year'        => $d->year,
+            ]);
+        }
+        Schema::connection('egecrm')->table('contracts', function (Blueprint $table) {
+            $table->dropColumn('id_contract');
+            $table->dropColumn('grade');
+            $table->dropColumn('id_student');
+            $table->dropColumn('year');
         });
     }
 
