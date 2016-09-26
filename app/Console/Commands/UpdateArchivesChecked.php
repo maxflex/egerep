ef729bcd111ef7946781eba5cabb4066e1a03d33
Max Kolyadin <makcyxa-k@yandex.ru>
2016-09-26 16:05:00 +0300

    Archive state update algo fix

--- app/Console/Commands/UpdateArchivesChecked.php
+++ app/Console/Commands/UpdateArchivesChecked.php
@@ -43,10 +43,10 @@ class UpdateArchivesChecked extends Command
 
         foreach($archives as $archive) {
             $x = $archive->total_lessons_missing + $archive->attachment->account_data_count;
-            if (! $x && $archive->date <= '2016-01-01') {
+            if (! $x && $archive->getOriginal('date') <= '2016-01-01') {
                 \DB::table('archives')->whereId($archive->id)->update(['state' => 'impossible']);
             }
-            if ($x && $archive->date <= '2015-01-01') {
+            if ($x && $archive->getOriginal('date') <= '2015-01-01') {
                 \DB::table('archives')->whereId($archive->id)->update(['state' => 'impossible']);
             }
             $bar->advance();
