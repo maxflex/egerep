4bb3612e94e6712480d1df786aa8f073f3de28b5
shamshod <shamik1551@mail.ru>
2016-09-28 11:01:27 +0300

    - hidden accounts changes

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -13,36 +13,74 @@ class AccountsController extends Controller
 {
     public function index($id)
     {
-        $tutor = Tutor::find($id);
-        $clients = $tutor->getAttachmenClients(0, false, true);
-        $hidden_clients_count = $tutor->clientsCount(1);
-
-        if (! count($clients) && !$hidden_clients_count) {
+        $data = $this->getData($id, [0, false, true]); // params in array: hidden, with_lesson_count, get_possible_archives
+        if (! count($data['clients']) && !$data['hidden_clients_count']) {
             return view('tutors.accounts.no_clients');
         }
-        return view('tutors.accounts.index')->with(ngInit([
-            'tutor_id'             => $id,
-            'clients'              => $clients,
-            'first_attachment_date'=> $tutor->getFirstAttachmentDate(),
-            'date_limit'           => $tutor->getDateLimit(),
-            'hidden_clients_count' => $hidden_clients_count,
-        ]) + ['tutor' => $tutor]);
+        return view('tutors.accounts.index')->with(ngInit($data) + ['tutor' => $data['tutor']]);
+
+//        $tutor = Tutor::find($id);
+//        $clients = $tutor->getAttachmenClients(0, false, true);
+//        $hidden_clients_count = $tutor->clientsCount(1);
+
+//        if (! count($clients) && !$hidden_clients_count) {
+//            return view('tutors.accounts.no_clients');
+//        }
+//        return view('tutors.accounts.index')->with(ngInit([
+//            'tutor_id'             => $id,
+//            'clients'              => $clients,
+//            'first_attachment_date'=> $tutor->getFirstAttachmentDate(),
+//            'date_limit'           => $tutor->getDateLimit(),
+//            'hidden_clients_count' => $hidden_clients_count,
+//        ]) + ['tutor' => $tutor]);
     }
 
     public function hidden($id)
     {
-        $tutor = Tutor::find($id);
-        $clients = $tutor->getAttachmenClients(1, true);
-
-        if (! count($clients)) {
+        $data = $this->getData($id, [1, true, false]); // params in array: hidden, with_lesson_count, get_possible_archives
+        if (! count($data['clients'])) {
             return view('shared.empty', [
                 'message' => 'у преподавателя нет скрытых клиентов'
             ]);
         }
-        return view('tutors.accounts.hidden')->with(ngInit([
-            'tutor_id'             => $id,
-            'clients'              => $clients,
-            'visible_clients_count' => $tutor->clientsCount(0),
-        ]) + ['tutor' => $tutor]);
+        return view('tutors.accounts.index')->with(ngInit($data) + ['tutor' => $data['tutor']]);
+
+//        $tutor = Tutor::find($id);
+//        $clients = $tutor->getAttachmenClients(1, true);
+//
+//        if (! count($clients)) {
+//            return view('shared.empty', [
+//                'message' => 'у преподавателя нет скрытых клиентов'
+//            ]);
+//        }
+//        return view('tutors.accounts.hidden')->with(ngInit([
+//            'tutor_id'             => $id,
+//            'clients'              => $clients,
+//            'visible_clients_count' => $tutor->clientsCount(0),
+//        ]) + ['tutor' => $tutor]);
+    }
+
+    private function getData($id, $attachmentParams = [])
+    {
+        list($hidden, $with_lesson_count, $get_possible_archives) = $attachmentParams;
+
+        $tutor = Tutor::find($id);
+        $clients = $tutor->getAttachmenClients($hidden, $with_lesson_count, $get_possible_archives);
+        $hidden_clients_count = $tutor->clientsCount(1);
+        $visible_clients_count = $tutor->clientsCount(0, true);
+
+        if (! count($clients) && !$hidden_clients_count) {
+            return view('tutors.accounts.no_clients');
+        }
+        return [
+                'page'                 => ($hidden ? 'hidden' : 'visible'),
+                'tutor_id'             => $id,
+                'tutor'                => $tutor,
+                'clients'              => $clients,
+                'first_attachment_date'=> $tutor->getFirstAttachmentDate(),
+                'date_limit'           => $tutor->getDateLimit(),
+                'hidden_clients_count' => $hidden_clients_count,
+                'visible_clients_count'=> $visible_clients_count,
+        ];
     }
 }
