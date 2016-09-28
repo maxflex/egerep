5c58bcafba58ffc42aed6ee66dc35d044a4ed193
shamshod <shamik1551@mail.ru>
2016-09-28 11:36:30 +0300

    - style fixes

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -18,21 +18,6 @@ class AccountsController extends Controller
             return view('tutors.accounts.no_clients');
         }
         return view('tutors.accounts.index')->with(ngInit($data) + ['tutor' => $data['tutor']]);
-
-//        $tutor = Tutor::find($id);
-//        $clients = $tutor->getAttachmenClients(0, false, true);
-//        $hidden_clients_count = $tutor->clientsCount(1);
-
-//        if (! count($clients) && !$hidden_clients_count) {
-//            return view('tutors.accounts.no_clients');
-//        }
-//        return view('tutors.accounts.index')->with(ngInit([
-//            'tutor_id'             => $id,
-//            'clients'              => $clients,
-//            'first_attachment_date'=> $tutor->getFirstAttachmentDate(),
-//            'date_limit'           => $tutor->getDateLimit(),
-//            'hidden_clients_count' => $hidden_clients_count,
-//        ]) + ['tutor' => $tutor]);
     }
 
     public function hidden($id)
@@ -44,20 +29,6 @@ class AccountsController extends Controller
             ]);
         }
         return view('tutors.accounts.index')->with(ngInit($data) + ['tutor' => $data['tutor']]);
-
-//        $tutor = Tutor::find($id);
-//        $clients = $tutor->getAttachmenClients(1, true);
-//
-//        if (! count($clients)) {
-//            return view('shared.empty', [
-//                'message' => 'у преподавателя нет скрытых клиентов'
-//            ]);
-//        }
-//        return view('tutors.accounts.hidden')->with(ngInit([
-//            'tutor_id'             => $id,
-//            'clients'              => $clients,
-//            'visible_clients_count' => $tutor->clientsCount(0),
-//        ]) + ['tutor' => $tutor]);
     }
 
     private function getData($id, $attachmentParams = [])
