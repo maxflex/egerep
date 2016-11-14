a3a9df484788e48f3ae8ad1a4e0cbef4ba8f0ce2
shamshod <shamik1551@mail.ru>
2016-11-14 18:25:11 +0300

    - tutors save button

--- resources/views/tutors/edit.blade.php
+++ resources/views/tutors/edit.blade.php
@@ -7,6 +7,9 @@
 @section('content')
 @section('controller', 'TutorsForm')
 @section('title-right')
+    <span style="position: absolute; left: 45%;" class="link-like link-reverse link-white" ng-show='!form_changed'>cохранено</span>
+    <span style="position: absolute; left: 45%;" class="link-like link-reverse link-white" ng-show="form_changed" ng-click="edit()" ng-disabled="saving">cохранить</span>
+
     <span class="header-link" ng-click='mergeTutor()'>склеить</span>
     <a target='_new' href="{{ $lk_link }}">режим просмотра</a>
     <a href="http://ege-repetitor.ru/tutors/person/@{{ tutor.id_a_pers }}" target="_blank">анкета на ege-repetitor.ru</a>
@@ -19,12 +22,6 @@
 
         @include('tutors._form')
 
-        <div class="row">
-            <div class="col-sm-12 center">
-                <button class="btn btn-primary" disabled ng-show="!form_changed">Сохранено</button>
-                <button class="btn btn-primary" ng-show="form_changed" ng-click="edit()" ng-disabled="saving">Сохранить</button>
-            </div>
-        </div>
     </div>
 </div>
 
