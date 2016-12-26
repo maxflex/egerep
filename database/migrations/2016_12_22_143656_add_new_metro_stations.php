6c2b02a2518221c378c18ad8b7df3492191dd56e
shamshod <shamik1551@mail.ru>
2016-12-26 13:24:43 +0300

    - new metro stations distances

--- database/migrations/2016_12_22_143656_add_new_metro_stations.php
+++ database/migrations/2016_12_22_143656_add_new_metro_stations.php
@@ -16,30 +16,30 @@ class AddNewMetroStations extends Migration
 
         DB::table('graph_places')
             ->insert([
-                ['name' => 'Петровско-Разумовская', 'lines' => '10', 'rel'=>'', 'type' => 'metro'], //212
-                ['name' => 'Фонвизинская',          'lines' => '10', 'rel'=>'', 'type' => 'metro'], //213
-                ['name' => 'Бутырская',             'lines' => '10', 'rel'=>'97', 'type' => 'metro'], //214
-                ['name' => 'Технопарк',             'lines' => '2', 'rel'=>'', 'type' => 'metro'], //215
-                ['name' => 'Бульвар Рокоссовского', 'lines' => '1', 'rel'=>'225', 'type' => 'metro'], //216
-                ['name' => 'Саларьево',             'lines' => '1', 'rel'=>'', 'type' => 'metro'], //217
-                ['name' => 'Спартак',               'lines' => '7', 'rel'=>'', 'type' => 'metro'], //218
-                ['name' => 'Балтийская',            'lines' => '14', 'rel'=>'29', 'type' => 'metro'], //219
-                ['name' => 'Лихоборы',              'lines' => '14', 'rel'=>'', 'type' => 'metro'], //220
-                ['name' => 'Владыкино',             'lines' => '14', 'rel'=>'27', 'type' => 'metro'], //221
-                ['name' => 'Ботанический сад',      'lines' => '14', 'rel'=>'20', 'type' => 'metro'], //222
-                ['name' => 'Ростокино',             'lines' => '14', 'rel'=>'', 'type' => 'metro'], //223
-                ['name' => 'Локомотив',             'lines' => '14', 'rel'=>'154', 'type' => 'metro'], //224
-                ['name' => 'Измайлово',             'lines' => '14', 'rel'=>'94', 'type' => 'metro'], //225
-                ['name' => 'Соколиная гора',        'lines' => '14', 'rel'=>'', 'type' => 'metro'], //226
-                ['name' => 'Шоссе Энтузиастов',     'lines' => '14', 'rel'=>'160', 'type' => 'metro'], //227
-                ['name' => 'Нижегородская',         'lines' => '14', 'rel'=>'', 'type' => 'metro'], //228
-                ['name' => 'Дубровка',              'lines' => '14', 'rel'=>'41', 'type' => 'metro'], //229
-                ['name' => 'Автозаводская',         'lines' => '14', 'rel'=>'2', 'type' => 'metro'], //230
-                ['name' => 'Площадь Гагарина',      'lines' => '14', 'rel'=>'65', 'type' => 'metro'], //231
-                ['name' => 'Лужники',               'lines' => '14', 'rel'=>'125', 'type' => 'metro'], //232
-                ['name' => 'Кутузовская',           'lines' => '14', 'rel'=>'64', 'type' => 'metro'], //233
-                ['name' => 'Шелепиха',              'lines' => '14', 'rel'=>'', 'type' => 'metro'], //234
-                ['name' => 'Панфиловская',          'lines' => '14', 'rel'=>'87', 'type' => 'metro'] //235
+                ['name' => 'Петровско-Разумовская', 'lines' => '10', 'rel'=>'97',   'type' => 'metro'], //212
+                ['name' => 'Фонвизинская',          'lines' => '10', 'rel'=>'',     'type' => 'metro'], //213
+                ['name' => 'Бутырская',             'lines' => '10', 'rel'=>'',     'type' => 'metro'], //214
+                ['name' => 'Технопарк',             'lines' => '2',  'rel'=>'',     'type' => 'metro'], //215
+                ['name' => 'Бульвар Рокоссовского', 'lines' => '1',  'rel'=>'',     'type' => 'metro'], //216
+                ['name' => 'Саларьево',             'lines' => '1',  'rel'=>'',     'type' => 'metro'], //217
+                ['name' => 'Спартак',               'lines' => '7',  'rel'=>'',     'type' => 'metro'], //218
+                ['name' => 'Балтийская',            'lines' => '14', 'rel'=>'29',   'type' => 'metro'], //219
+                ['name' => 'Лихоборы',              'lines' => '14', 'rel'=>'',     'type' => 'metro'], //220
+                ['name' => 'Владыкино',             'lines' => '14', 'rel'=>'27',   'type' => 'metro'], //221
+                ['name' => 'Ботанический сад',      'lines' => '14', 'rel'=>'20',   'type' => 'metro'], //222
+                ['name' => 'Ростокино',             'lines' => '14', 'rel'=>'',     'type' => 'metro'], //223
+                ['name' => 'Локомотив',             'lines' => '14', 'rel'=>'154',  'type' => 'metro'], //224
+                ['name' => 'Измайлово',             'lines' => '14', 'rel'=>'94',   'type' => 'metro'], //225
+                ['name' => 'Соколиная гора',        'lines' => '14', 'rel'=>'',     'type' => 'metro'], //226
+                ['name' => 'Шоссе Энтузиастов',     'lines' => '14', 'rel'=>'160',  'type' => 'metro'], //227
+                ['name' => 'Нижегородская',         'lines' => '14', 'rel'=>'',     'type' => 'metro'], //228
+                ['name' => 'Дубровка',              'lines' => '14', 'rel'=>'41',   'type' => 'metro'], //229
+                ['name' => 'Автозаводская',         'lines' => '14', 'rel'=>'2',    'type' => 'metro'], //230
+                ['name' => 'Площадь Гагарина',      'lines' => '14', 'rel'=>'65',   'type' => 'metro'], //231
+                ['name' => 'Лужники',               'lines' => '14', 'rel'=>'125',  'type' => 'metro'], //232
+                ['name' => 'Кутузовская',           'lines' => '14', 'rel'=>'64',   'type' => 'metro'], //233
+                ['name' => 'Шелепиха',              'lines' => '14', 'rel'=>'',     'type' => 'metro'], //234
+                ['name' => 'Панфиловская',          'lines' => '14', 'rel'=>'87',   'type' => 'metro']  //235
             ]);
 
         DB::table('stations')
@@ -69,6 +69,48 @@ class AddNewMetroStations extends Migration
                 ['title' => 'Шелепиха',              'line_id' => '14', 'lat'=> 55.7575, 'lng' => 37.5256], //234
                 ['title' => 'Панфиловская',          'line_id' => '14', 'lat'=> 55.7980, 'lng' => 37.4998] //235
             ]);
+
+        DB::table('graph_distances')
+            ->insert([
+                ['from' => 212, 'to' => 97,   'distance' => 1],
+                ['from' => 212, 'to' => 213,  'distance' => 2],
+                ['from' => 213, 'to' => 214,  'distance' => 3],
+                ['from' => 215, 'to' => 2,    'distance' => 2],
+                ['from' => 215, 'to' => 50,   'distance' => 2],
+                ['from' => 216, 'to' => 154,  'distance' => 3],
+                ['from' => 217, 'to' => 210,  'distance' => 2],
+                ['from' => 218, 'to' => 162,  'distance' => 3],
+                ['from' => 219, 'to' => 29,   'distance' => 12],
+                ['from' => 219, 'to' => 220,  'distance' => 5],
+                ['from' => 219, 'to' => 235,  'distance' => 5],
+                ['from' => 220, 'to' => 235,  'distance' => 5],
+                ['from' => 220, 'to' => 221,  'distance' => 5],
+                ['from' => 221, 'to' => 222,  'distance' => 3],
+                ['from' => 221, 'to' => 27,   'distance' => 7],
+                ['from' => 222, 'to' => 20,   'distance' => 8],
+                ['from' => 222, 'to' => 223,  'distance' => 3],
+                ['from' => 223, 'to' => 224,  'distance' => 8],
+                ['from' => 223, 'to' => 154,  'distance' => 5],
+                ['from' => 224, 'to' => 225,  'distance' => 2],
+                ['from' => 225, 'to' => 94,   'distance' => 9],
+                ['from' => 225, 'to' => 226,  'distance' => 3],
+                ['from' => 226, 'to' => 227,  'distance' => 3],
+                ['from' => 227, 'to' => 160,  'distance' => 8],
+                ['from' => 227, 'to' => 228,  'distance' => 5],
+                ['from' => 228, 'to' => 229,  'distance' => 7],
+                ['from' => 229, 'to' => 41,   'distance' => 12],
+                ['from' => 229, 'to' => 230,  'distance' => 2],
+                ['from' => 230, 'to' => 231,  'distance' => 10],
+                ['from' => 230, 'to' => 2,    'distance' => 10],
+                ['from' => 231, 'to' => 232,  'distance' => 4],
+                ['from' => 231, 'to' => 65,   'distance' => 6],
+                ['from' => 232, 'to' => 125,  'distance' => 8],
+                ['from' => 232, 'to' => 233,  'distance' => 3],
+                ['from' => 233, 'to' => 64,   'distance' => 6],
+                ['from' => 233, 'to' => 234,  'distance' => 4],
+                ['from' => 234, 'to' => 235,  'distance' => 8],
+                ['from' => 235, 'to' => 87,   'distance' => 13]
+            ]);
     }
 
     /**
@@ -78,6 +120,24 @@ class AddNewMetroStations extends Migration
      */
     public function down()
     {
-        //
+        DB::table('graph_places')
+            ->whereIn('id', range(212, 235))
+            ->delete();
+
+        DB::statement("alter table graph_places auto_increment = 212");
+
+        DB::table('stations')
+            ->whereIn('id', range(212, 235))
+            ->delete();
+
+        DB::statement("alter table stations auto_increment = 212");
+
+        DB::table('graph_distances')
+            ->whereIn('from', range(212, 235))
+            ->delete();
+
+        DB::table('graph_distances')
+            ->whereIn('to', range(212, 235))
+            ->delete();
     }
 }
