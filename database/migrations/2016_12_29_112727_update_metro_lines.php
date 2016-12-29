ca43f7e67ccc8f01bd432ce901e5f98a63e1de6a
Max Kolyadin <makcyxa-k@yandex.ru>
2016-12-29 11:51:10 +0300

    Station line duplicate migration

--- database/migrations/2016_12_29_112727_update_metro_lines.php
+++ database/migrations/2016_12_29_112727_update_metro_lines.php
@@ -12,7 +12,35 @@ class UpdateMetroLines extends Migration
      */
     public function up()
     {
-        //
+        \DB::table('stations')->whereId(188)->update([
+            'lat' => '55.7576',
+            'lng' => '37.6577'
+        ]);
+        \DB::table('stations')->whereId(63)->update([
+            'lng' => '37.6595',
+            'line_id' => 5
+        ]);
+        \DB::table('stations')->whereId(47)->update([
+            'line_id' => 5
+        ]);
+        \DB::table('stations')->whereId(15)->update([
+            'line_id' => 5
+        ]);
+        \DB::table('stations')->whereId(51)->update([
+            'line_id' => 5
+        ]);
+        \DB::table('stations')->whereId(197)->update([
+            'line_id' => 11
+        ]);
+        \DB::table('stations')->whereId(62)->update([
+            'line_id' => 4
+        ]);
+        \DB::table('stations')->whereId(91)->update([
+            'line_id' => 5
+        ]);
+        \DB::table('stations')->whereId(92)->update([
+            'line_id' => 5
+        ]);
     }
 
     /**
