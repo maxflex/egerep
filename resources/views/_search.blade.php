76d05b75b187b4c3bcc55e9d40de5f2530bac875
Max Kolyadin <makcyxa-k@yandex.ru>
2017-01-09 19:19:41 +0300

    Search update

--- resources/views/_search.blade.php
+++ resources/views/_search.blade.php
@@ -12,13 +12,11 @@
 
                 <div v-if="results > 0" v-for="(index, row) in lists" class="resultRow" v-bind:class="{ active: ((index+1) ==  active)}">
                     <div v-if="row.type == 'clients'">
-                        <a :href="row.link" v-html="'клиент ' + row.id" target="_blank">клиент @{{ row.id }}</a>
+                        <a :href="row.link" v-html="'клиент ' + row.id" target="_blank"></a>
                     </div>
                     <div v-else>
-                        <a :href="row.link" target="_blank" v-html="'репетитор ' + row.last_name + ' ' + row.first_name + ' ' + row.middle_name">репетитор
-                            <span v-if="row.first_name != ''">@{{ row.last_name + ' ' + row.first_name + ' ' + row.middle_name }}</span>
-                            <span v-else>имя не указано</span>
-                        </a>
+                        <a v-if="row.first_name" :href="row.link" target="_blank" v-html="'репетитор ' + row.last_name + ' ' + row.first_name + ' ' + row.middle_name"></a>
+                        <a v-else :href="row.link" target="_blank" v-html="'репетитор имя не указано'"></a>
                     </div>
                 </div>
 
