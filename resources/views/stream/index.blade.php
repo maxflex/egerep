e667f94828f91f5ff172c7d67bd011d61900692c
Max Kolyadin <makcyxa-k@yandex.ru>
2017-03-15 18:07:14 +0300

    date filter added to stream

--- resources/views/stream/index.blade.php
+++ resources/views/stream/index.blade.php
@@ -34,6 +34,24 @@
                 value="@{{ t }}">@{{ t }}</option>
         </select>
     </div>
+    <div>
+        <div class="form-group">
+            <div class="input-group custom">
+              <span class="input-group-addon">начало –</span>
+              <input type="text" readonly ng-change='filter()'
+                  class="form-control bs-date-clear pointer" ng-model="search.date_start">
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="form-group">
+            <div class="input-group custom">
+              <span class="input-group-addon">конец –</span>
+              <input type="text" readonly ng-change='filter()'
+                  class="form-control bs-date-clear pointer" ng-model="search.date_end">
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="row">
