cf50d8ddb870a600ab96088a2647c75e78917f3b
Max Kolyadin <makcyxa-k@yandex.ru>
2017-03-23 15:49:09 +0300

    migrate debts index

---
+++ database/migrations/2017_03_23_154843_add_index_to_debts_date.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexToDebtsDate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('debts', function (Blueprint $table) {
+            $table->index('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('debts', function (Blueprint $table) {
+            //
+        });
+    }
+}
