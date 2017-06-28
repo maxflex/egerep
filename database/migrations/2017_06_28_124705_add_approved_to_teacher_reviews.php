096e659d345bd20f03005cf0d32f74e61cf8afe8
Max Kolyadin <makcyxa-k@yandex.ru>
2017-06-28 12:47:36 +0300

    Migrate egecrm

---
+++ database/migrations/2017_06_28_124705_add_approved_to_teacher_reviews.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddApprovedToTeacherReviews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('egecrm')->table('teacher_reviews', function (Blueprint $table) {
+            $table->boolean('approved')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teacher_reviews', function (Blueprint $table) {
+            //
+        });
+    }
+}
