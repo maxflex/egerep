bf3b3ca81704037e031c5919408fe2ca1fd805eb
Max Kolyadin <makcyxa-k@yandex.ru>
2017-07-23 14:35:55 +0300

    Add created_at to tutors

---
+++ database/migrations/2017_07_23_140703_add_created_at_to_tutors.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCreatedAtToTutors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tutors', function (Blueprint $table) {
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tutors', function (Blueprint $table) {
+            //
+        });
+    }
+}
