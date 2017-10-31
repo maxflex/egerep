bad445a59400005ad340a64f46113875a0045679
Max Kolyadin <makcyxa-k@yandex.ru>
2017-10-31 17:33:58 +0300

    remainders update

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -210,8 +210,8 @@ class PaymentsController extends Controller
                 'sum'       => $source->remainder,
                 'comment'   => 'входящий остаток',
             ];
-            if (! isset($items[$date])) {
-                $items[$date] = [];
+            if (! isset($items[$source->remainder_date])) {
+                $items[$source->remainder_date] = [];
                 ksort($items);
                 $items = array_reverse($items);
             }
