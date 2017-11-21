74b69739db40574d52db86251fc930bacee08186
Max Kolyadin <makcyxa-k@yandex.ru>
2017-11-21 17:53:53 +0300

    migrate egecrm

---
+++ database/migrations/2017_11_21_174949_egecrm_drop_official_price_from_vj.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EgecrmDropOfficialPriceFromVj extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('egecrm')->table('visit_journal', function (Blueprint $table) {
+            $table->dropColumn('insurance');
+            $table->dropColumn('teacher_price_official');
+        });
+        Schema::connection('egecrm')->table('groups', function (Blueprint $table) {
+            $table->dropColumn('teacher_price_official');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('visit_journal', function (Blueprint $table) {
+            //
+        });
+    }
+}
