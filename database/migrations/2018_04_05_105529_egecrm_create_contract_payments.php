e21fa43192c84b263943d919915dbc9d4ff85449
maxflex <makcyxa-k@yandex.ru>
2018-04-06 13:11:22 +0300

    Migrate egecrm

--- database/migrations/2018_04_05_105529_egecrm_create_contract_payments.php
+++ database/migrations/2018_04_05_105529_egecrm_create_contract_payments.php
@@ -12,12 +12,12 @@ class EgecrmCreateContractPayments extends Migration
      */
     public function up()
     {
-        Schema::connection('egecrm')->create('contract_payments', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('id_contract')->unsigned()->index();
-            $table->date('date')->nullable();
-            $table->integer('sum');
-        });
+        // Schema::connection('egecrm')->create('contract_payments', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->integer('id_contract')->unsigned()->index();
+        //     $table->date('date')->nullable();
+        //     $table->integer('sum');
+        // });
 
         // Договоры
         dbEgecrm('contract_payments')->truncate();
@@ -27,17 +27,22 @@ class EgecrmCreateContractPayments extends Migration
         foreach($contracts as $contract) {
             if ($contract->payments_split) {
                 $contract_info = dbEgecrm('contract_info')->where('id_contract', $contract->id_contract)->first();
-                $data = [
-                    'id_contract' => $contract->id,
-                    'sum' => intval($contract->discount > 0 ? ($contract->sum * ((100 - $contract->discount) / 100)) : $contract->sum)
-                ];
-                dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => null]));
+                $sum = intval($contract->discount > 0 ? ($contract->sum * ((100 - $contract->discount) / 100)) : $contract->sum);
+                $data = ['id_contract' => $contract->id];
                 switch($contract->payments_split) {
+                    case 1: {
+                        $data['sum'] = $sum;
+                        dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => null]));
+                    }
                     case 2: {
+                        $data['sum'] = round($sum / 2);
+                        dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => null]));
                         dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => ($contract_info->year + 1) . '-01-27']));
                         break;
                     }
                     case 3: {
+                        $data['sum'] = round($sum / 3);
+                        dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => null]));
                         if ($contract->payments_queue) {
                             dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => $contract_info->year . '-11-27']));
                             dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => ($contract_info->year + 1) . '-02-27']));
@@ -48,6 +53,8 @@ class EgecrmCreateContractPayments extends Migration
                         break;
                     }
                     case 8: {
+                        $data['sum'] = round($sum / 8);
+                        dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => null]));
                         dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => $contract_info->year . '-10-15']));
                         dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => $contract_info->year . '-11-15']));
                         dbEgecrm('contract_payments')->insert(array_merge($data, ['date' => $contract_info->year . '-12-15']));
