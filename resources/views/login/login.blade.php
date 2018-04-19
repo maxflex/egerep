655b2cb3a7dae181a041434e219d2578b6f4f7d4
maxflex <makcyxa-k@yandex.ru>
2018-04-19 18:10:23 +0300

    Background edit

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,20 +1,20 @@
 @extends('login')
 @section('content')
-<center autocomplete="off" class="animated fadeIn" style='animation-duration: 1.5s'>
+<center autocomplete="off" class="animated fadeIn">
         <div style='display: none'>
             <input type="password"/>
         </div>
-        <div class="input-wrapper">
+        <div class="input-wrapper" style='animation-duration: 1.5s'>
             <input ng-disabled="sms_verification" type="text" id="inputLogin" placeholder="login" autofocus ng-model="login" autocomplete="off" ng-keyup="enter($event)">
         </div>
-        <div class="input-wrapper">
+        <div class="input-wrapper" style='animation-duration: 1.5s'>
             <input ng-class="{'password-start-typing': password}" ng-disabled="sms_verification" type="password" id="inputPassword"  placeholder="password" ng-model="password" autocomplete="off" ng-keyup="enter($event)">
             <span class="login-icons" ng-show="!sms_verification && password">
                 <img ng-show="in_process"  class="in-process"  src="img/svg/spinner.svg">
                 <img ng-show="!in_process" class="login-arrow" src="img/svg/next.svg" ng-click="checkFields()">
             </span>
         </div>
-        <div class="input-wrapper" ng-show="sms_verification" style='position: absolute'>
+        <div class="input-wrapper" ng-show="sms_verification" style='position: absolute' style='animation-duration: 1.5s'>
             <input type="text" id="sms-code" placeholder="sms code" ng-model="code" autocomplete="off" ng-keyup="enter($event)">
             <span class="login-icons" ng-show="code">
                 <img ng-show="in_process"  class="in-process"  src="img/svg/spinner.svg">
