fca45ac9f64128bfe7b476bccd5efc8f6a2d68fa
maxflex <makcyxa-k@yandex.ru>
2018-06-26 19:07:51 +0300

    Phones sms delivery

--- app/Console/Commands/SendSms.php
+++ app/Console/Commands/SendSms.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Sms;
 use App\Models\Email;
-use Cache;
+use Redis;
 
 class SendSms extends Command
 {
@@ -40,20 +40,13 @@ class SendSms extends Command
      */
     public function handle()
     {
-        // $this->info(getcwd());
-        // отослано 400
+        $cache_key = 'sms_already_sent';
 
         // отправлять по
-        $per_send = 3;
-
-        // сколько хранить в кеше
-        $keep_for = 60 * 24 * 7;
+        $per_send = 100;
 
         // уже отослано
-        $already_sent = Cache::remember('sms_already_sent', $keep_for, function() {
-            return 0;
-        });
-        $this->info($already_sent);
+        $already_sent = Redis::get($cache_key) ?: 0;
 
         $text = "ЕГЭ-Центр ведет набор 10-11 классников в школу с уклоном на подготовку к ЕГЭ. Возможен экстернат. Подробности на ege-centr.ru или по тел.: +7 (495) 646-85-92";
         $phones = file_get_contents('phones.tsv');
@@ -71,6 +64,6 @@ class SendSms extends Command
             Sms::send($phone, $message, false);
         }
 
-        Cache::put('sms_already_sent', $already_sent + $per_send);
+        Redis::set($cache_key, $already_sent + $per_send);
     }
 }
